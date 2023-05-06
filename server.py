0b5d98754958935ba2b2b499d8199384be110db9
pafnootiy <ksander-86@mail.ru>
2023-05-06 16:53:16 +0400

    dd timer block.

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 from livereload import Server
 
-
 server = Server()
 
+# Добавить файл для отслеживания изменений
 server.watch('index.html')
+server.watch('static/styles.css')
+server.watch('static/player.js')
 
+# Обслуживание текущего каталога (где должен находиться index.html)
 server.serve(root='.')
